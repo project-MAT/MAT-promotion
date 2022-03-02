f73ea305c4d227d47739d3109ca5663d6459a53d
yoosion030 <rsy5487@naver.com>
2022-03-02 18:23:47 +0900

    Add Section1

---
+++ components/Section1/index.js
@@ -0,0 +1,18 @@
+import * as S from "./style";
+import * as I from "../../Assets/SVG";
+
+const Section1 = () => {
+  return (
+    <S.MainSection>
+      <S.Logo>
+        <I.Logo />
+      </S.Logo>
+      <S.Title>
+        프로젝트에 필요한 멤버를 쉽고 빠르게, <S.Yellow>Make A Team</S.Yellow>
+      </S.Title>
+      <S.More>더 알아보기</S.More>
+    </S.MainSection>
+  );
+};
+
+export default Section1;
---
+++ components/Section1/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const MainSection = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Logo = styled.div`
+  margin-top: 250px;
+`;
+
+export const Title = styled.div`
+  font-size: 30px;
+  margin-bottom: 290px;
+`;
+
+export const Yellow = styled.span`
+  color: #ffbf19;
+`;
+
+export const More = styled.div`
+  font-size: 20px;
+`;
